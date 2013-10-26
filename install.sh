81010acde98175a9acc8cfc2e2101990111a79ff
swapnilsm <swapnilsm@gmail.com>
2013-10-26 22:04:54 +0530

    after accidental deletion... First commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bash/sh -e
+
+##########################################
+# Terminal Case Insensitive Autocomplete #
+##########################################
+
+sudo echo "set completion-ignore-case On" >> ~/.inputrc
