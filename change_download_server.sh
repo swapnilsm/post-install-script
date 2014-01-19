223cb562e2e4bf48ffebd9635a8ab9b9dd797902
Swapnil S. Mahajan <swapnilsm@gmail.com>
2014-01-20 03:46:16 +0530

    Added fix for initial install

--- change_download_server.sh
+++ change_download_server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -e
 if [ "$1" = "iitk" ]; then
     sudo sed -i s:"np.archive.ubuntu.com":"mirror.cse.iitk.ac.in": /etc/apt/sources.list
+    sudo sed -i s:"in.archive.ubuntu.com":"mirror.cse.iitk.ac.in": /etc/apt/sources.list    
 else
     sudo sed -i s:"mirror.cse.iitk.ac.in":"np.archive.ubuntu.com": /etc/apt/sources.list
+    sudo sed -i s:"in.archive.ubuntu.com":"np.archive.ubuntu.com": /etc/apt/sources.list    
 fi
+sudo apt-get update
